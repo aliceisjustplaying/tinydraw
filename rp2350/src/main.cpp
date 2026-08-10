ef52e2861060dc6256bb6bfb786734e54b75e3d4
alice <aliceisjustplaying@gmail.com>
2026-08-10 22:27:41 +0100

    fix: smooth RP2350 touch centerline

--- rp2350/src/main.cpp
+++ rp2350/src/main.cpp
@@ -393,6 +393,7 @@ int main() {
 
   tinydraw::InkConfig brush;
   brush.size = tinydraw::brush_size(toolbar.size);
+  brush.streamline = 0.65F;
   tinydraw::InkStream ink(brush);
   bool touch_down = false;
   bool toolbar_gesture = false;
