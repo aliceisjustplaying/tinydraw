d725498eb93db0a23ab06d52aed3024963422853
alice <aliceisjustplaying@gmail.com>
2026-08-18 19:59:05 +0100

    refactor(vector-v2): separate chrome input handling

--- vector_v2/sources.cmake
+++ vector_v2/sources.cmake
@@ -2,6 +2,7 @@ set(TINYDRAW_VECTOR_V2_SRCS
   "${CMAKE_CURRENT_LIST_DIR}/src/authority_journal.cpp"
   "${CMAKE_CURRENT_LIST_DIR}/src/chained_operation_builder.cpp"
   "${CMAKE_CURRENT_LIST_DIR}/src/chrome.cpp"
+  "${CMAKE_CURRENT_LIST_DIR}/src/chrome_input.cpp"
   "${CMAKE_CURRENT_LIST_DIR}/src/frame_scroller.cpp"
   "${CMAKE_CURRENT_LIST_DIR}/src/idle_repair.cpp"
   "${CMAKE_CURRENT_LIST_DIR}/src/incremental_document.cpp"
